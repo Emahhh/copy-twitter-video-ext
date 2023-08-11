24c28a68c182e47feb19f4cf617c34f93c659dbf
Emanuele B <buonaccorsiemanuele@gmail.com>
2023-08-11 18:41:26 +0200

    works on full page

--- foreground.js
+++ foreground.js
@@ -4,4 +4,60 @@
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+function writeOnClipboard(newClip) {
+    navigator.clipboard.writeText(newClip).then(
+        () => {
+            /* clipboard successfully set */
+            alert(`Copied succesfully to clipboard:\n${newClip}`);
+        },
+        () => {
+            alert(`Error while copying this text:\n${newClip}`);
+        }
+    );
+}
+
+function getMediaURL() {
+    let mediaURLs = document.title.match(/(\bhttps?:\/\/t\.co\/[\w-\.]+)/g);
+    let mediaURL = mediaURLs?.slice(-1)[0];
+    return mediaURL;
+}
+
+function copyLinkInTitle() {
+    let mediaURL = getMediaURL();
+    if (mediaURL) {
+        writeOnClipboard(mediaURL);
+    } else {
+        alert('No media URL found');
+    }
+}
+
+
+
+
+async function tryAddButton() {
+    if (getMediaURL()) {
+        let button = document.createElement("button");
+        button.innerHTML = "Copy video URL";
+        button.addEventListener("click", copyLinkInTitle);
+        // Style
+        button.style.position = "fixed";
+        button.style.bottom = "0";
+        button.style.right = "0";
+        button.style.zIndex = "9999";
+
+        document.body.appendChild(button);
+    } else {
+        alert('No media URL found! I found this: ' + document.title);
+    }
+}
+
+
+    let waitForTitleInterval = setInterval(function () {
+        if (document.title) {
+            clearInterval(waitForTitleInterval); // Stop the interval once the title is loaded
+            tryAddButton();
+        } else {
+            console.log('Waiting for title');
+        }
+    }, 100);
+
